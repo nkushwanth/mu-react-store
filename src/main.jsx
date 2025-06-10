0559248cb9b0482418596cb5db40eb2e82f8448d
nkushwanth <nagamkushwanthreddy@gmail.com>
2025-06-10 16:36:38 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+
 import { registerSW } from 'virtual:pwa-register';
 
 const updateSW = registerSW({

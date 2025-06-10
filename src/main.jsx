eaa6db1860db2294601968f45345656e1c439a74
nkushwanth <nagamkushwanthreddy@gmail.com>
2025-06-10 16:02:35 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { registerSW } from 'virtual:pwa-register';
 
-createRoot(document.getElementById('root')).render(
-<div>
- 
-<App/>
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline.");
+  }
+});
 
-</div>
-);
+createRoot(document.getElementById("root")).render(<App />);
